acee807643c71a5d489efbd9f3f843cc9ac8a60d
muhsupriazis <muhsupriazis@gmail.com>
2023-10-15 22:29:24 +0700

    fix:baseurl

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+async function getData() {
+  const res = await fetch(`${process.env.VERCEL_URL}/api/posts`)
+  return res.json()
+}
+
 export default async function Home() {
+  const posts = await getData();
+  
   return (
     <>
       <h1>Hello World</h1>
